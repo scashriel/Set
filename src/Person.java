ebbf0a96a5e830453caadd61666c54ae1567d132
Shirah Cashriel <scashriel@gmail.com:wq>
2020-01-13 17:26:48 +0200

    skeleton

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,17 @@
-public class Person {
+public class Person implements Comparable<Person>{
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    //constructor
+    public Person(String id, String firstName, String lastName, int birthYear){
+        //implement
+
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return 0;
+    }
 }
