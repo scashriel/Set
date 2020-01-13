25bc1eea1f5a24b4be9edd4ae9f0390d2a47ed75
Shirah Cashriel <scashriel@gmail.com:wq>
2020-01-13 18:37:45 +0200

    fixed function

--- src/getMin.java
+++ src/getMin.java
@@ -1,16 +1,11 @@
 //a class that returns min element in a set
 
-public class getMin{//<T extends Comparable<T>> implements Comparable<T> {
-//
-//    @Override
-//    public int compareTo(T o) {
-//       return this.compareTo(o);
-//    }
+public class getMin{
 
     public static <T extends Comparable<T>> T getMin(SetClass<T> s){
-        T min = s.getSetElement(1);
+        T min = s.getSetElement(1); //set min to first element in set
         for (T e : s){
-            if(e.compareTo(min) == -1){
+            if(e.compareTo(min) < -1){
                 min = e;
             }
         }
