1216571509a328e676484dc7a3985fdf101830dd
Shirah Cashriel <scashriel@gmail.com:wq>
2020-01-13 16:32:44 +0200

    checkout to devSetClass

--- src/Comparing.java
+++ src/Comparing.java
@@ -1,9 +1,21 @@
-////a class that
-//
-//public class Comparing<T> implements Comparable<T> {
-//
-//    @Override
-//    public <T> compareTo(T o) {
-//        return ;
-//    }
-//}
+//a class that
+
+public class Comparing<T> implements Comparable<T> {
+
+    @Override
+    public int compareTo(T o) {
+        int min = 1;
+        if(this.equals(o)){
+            min = 0;
+        }
+        else if (this < o){
+            min = -1;
+        }
+        return min;
+    }
+
+    public T getMin(SetClass<T> s){
+
+
+    }
+}
