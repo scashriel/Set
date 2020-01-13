e55e389e9bc655f061a178c5f4e9f24108ab9868
Shirah Cashriel <scashriel@gmail.com:wq>
2020-01-13 17:44:36 +0200

    implement functions

--- src/Person.java
+++ src/Person.java
@@ -6,12 +6,39 @@ public class Person implements Comparable<Person>{
 
     //constructor
     public Person(String id, String firstName, String lastName, int birthYear){
-        //implement
-
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
     }
 
     @Override
     public int compareTo(Person o) {
-        return 0;
+        if(this.lastName.equals(o.lastName)){
+            return (this.firstName.compareTo(this.firstName));
+        }
+        else
+            return this.lastName.compareTo(o.lastName);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getBirthYear() {
+        return this.birthYear;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s %s was born in %d and their id number is %s",this.firstName,this.lastName, this.birthYear, this.id);
     }
 }
