ca939d9184ecdbe2a05ae5a9ffd1bdc51517e8cd
Shirah Cashriel <scashriel@gmail.com:wq>
2020-01-13 18:57:18 +0200

    fixed conflict

--- src/getMin.java
+++ src/getMin.java
@@ -5,7 +5,7 @@ public class getMin{
     public static <T extends Comparable<T>> T getMin(SetClass<T> s){
         T min = s.getSetElement(0); //set min to first element in set
         for (T e : s){
-            if(e.compareTo(min) < -1){
+            if(e.compareTo(min) < 0){
                 min = e;
             }
         }
